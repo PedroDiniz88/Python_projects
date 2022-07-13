7221699edcc101dd10bca13561cb3ac717aee0c3
Pedro Damasceno <104572156+PedroDiniz88@users.noreply.github.com>
2022-07-13 13:24:55 -0300

    Lista de Convidados
    - Lê senha e nome de usuario;
    - Verifica se a senha está correta;
    - Imprime a lista salva, se o usuário desejar;
    - Se o usuário não quiser imprimir a lista, encerra o programa;

---
+++ Lista de convidados.py
@@ -0,0 +1,23 @@
+print ("Programa LISTA DE CONVIDADOS")
+pw = 0
+senha = '3715'
+nome = input('digite o nome de usuario: ')
+while (pw != senha):
+    pw = input('Digite sua senha: ')
+    if senha == pw:
+        print("Senha correta")
+    else:
+        print('Sua incorreta. Tente novamente')
+print ('Bem vindo',nome,'\nVocê entrou no sistema.')
+lista_nomes = ['Andre', 'Thiago', 'João', 'Lucas']
+resp = 0
+S = str('s')
+while (resp != S):
+    print('Deseja imprimir sua lista de convidados?')
+    resp = str(input('Digite S/N:'))
+    if resp == S:
+        print(lista_nomes)
+        print('Programa finalizado')
+    else:
+        print('Programa finalizado')
+        break
